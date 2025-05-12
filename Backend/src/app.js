94750e538e8a3339dd851bed33e9b1d96cb33fba
Umashnkar Uikey <umashankaruikey325@gmail.com>
2025-05-12 19:37:34 +0530

    origin add

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -6,7 +6,12 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors());
+app.use(cors(
+  {
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  }
+));
 
 app.use("/project", projectRoutes);
 app.use("/code", codeRoutes);
