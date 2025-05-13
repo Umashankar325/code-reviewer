e467ea57b22f8175a2c3cd1c4b47388bf1242627
Umashnkar Uikey <umashankaruikey325@gmail.com>
2025-05-13 19:11:54 +0530

    chang in db

--- Backend/src/db/db.js
+++ Backend/src/db/db.js
@@ -3,6 +3,9 @@ import config from "../config/config.js";
 const connect = async () => {
   try {
     await mongoose.connect(config.MONGODB_URI);
+    await mongoose.connect(process.env.MONGODB_URI);
+    // console.log(config.MONGODB_URI);
+    
 
     console.log("db connected succsefully");
   } catch (error) {
